11d58498d055f9ecf4b775b8c6f4b50a326be37c
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2022-12-11 00:00:12 +0200

    git push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 public class Main {
+    String s1 = "Dangerous Zone";
+    String s2 = "Safe Zone";
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter no of numbers");
@@ -7,8 +9,6 @@ public class Main {
         System.out.println("Enter numbers");
         int largest = Integer.MIN_VALUE;
         int smallest = Integer.MAX_VALUE;
-        String s1 = "Dangerous Zone";
-        String s2 = "Safe Zone";
         for (int i = 0; i < j; i++)
         {
             int x = in.nextInt();
@@ -23,20 +23,26 @@ public class Main {
         }
         int difference = largest - smallest;
         System.out.print("The Difference is :" + difference);
-        method(difference,largest);
-
+        boolean b=method(difference,largest);
+        if(b==true)
+        {
+            System.out.print("s1 :");
+        }
+        else
+        {
+            System.out.print("s2 :");
+        }
     }
 
     public static boolean method(int difference , int largest)
     {
-
         if (difference < (largest / 2))
         {
           return true;
         }
         else
         {
-           return false;
+         return false;
         }
     }
 }
