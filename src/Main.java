ff93469bc611f683a8781fa7e02b4129440f6142
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2022-12-14 23:50:47 +0200

    git push

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Main {
-   static String s1 = "Dangerous Zone";
+    static String s1 = "Dangerous Zone";
     static String s2 = "Safe Zone";
+    static String s = "Special Zone";
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter no of numbers");
@@ -9,46 +11,82 @@ public class Main {
         System.out.println("Enter numbers");
         int largest = Integer.MIN_VALUE;
         int smallest = Integer.MAX_VALUE;
-        for (int i = 0; i < j; i++)
-        {
+        for (int i = 0; i < j; i++) {
             int x = in.nextInt();
-            if(i==0)
-            {
-                largest=x;
-                smallest=x;
-            }
-            if (x > largest)
-            {
+            if (i == 0) {
                 largest = x;
+                smallest = x;
             }
-            else if (x < smallest)
-            {
+            if (x > largest) {
+                largest = x;
+            } else if (x < smallest) {
                 smallest = x;
             }
         }
-        int difference=0;
+        int difference = 0;
         difference = largest - smallest;
         System.out.println("The Difference is :" + difference);
-        boolean b=method(difference,largest);
-        if(b==true)
-        {
-            System.out.println(s1);
+        boolean h = special(largest, smallest, difference, j);
+        if (h == true) {
+            System.out.println(s);
         }
-        else
-        {
+        boolean b = method(difference, largest);
+        if (b == true) {
+            System.out.println(s1);
+        } else {
             System.out.print(s2);
         }
     }
 
-    public static boolean method(int difference , int largest)
-    {
-        if (difference < (largest / 2))
+    public static boolean method(int difference, int largest) {
+        if (difference < (largest / 2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean special(int largest, int smallest, int difference, int value) {
+        if (difference == 0)
         {
-          return true;
+            for (int i = 0; i < value; i++)
+            {
+                if (largest - i == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
         else
         {
-         return false;
+            int count1 = 0;
+            int count2 = 0;
+            for (int i = 0; i < value; i++)
+            {
+                if (i-largest==0)
+                {
+                    count1++;
+                    return true;
+                }
+                else if (i-smallest==0)
+                {
+                    count2++;
+                    return true;
+                }
+            }
+            if ((count1 == (value - 1) && count2 == 1) || (count1 == 1 && count2 ==(value-1)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+        return true;
     }
 }
